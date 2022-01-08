e21035c87763c858dbc35541a8f39ebebf3ad3ce
Michael Bashford <mikebashford@gmail.com>
2022-01-08 10:05:09 -0600

    Initial commit with questions added and inquirer installed

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = ['What is the title of the project?', 'Describe what the project does.', 'How do I install this project?', 'How do I use this project?', 'Who contributed to the project?', 'How do I use the test functions involved in the project?', 'Which license would you like to add?', 'What is your github username?', 'What is your email?'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
